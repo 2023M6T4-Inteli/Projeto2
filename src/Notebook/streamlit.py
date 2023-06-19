f9a7ad8bdc7f5f54f205264e8807f1ca9f6b64c6
RodrigoMMartins23 <rodrigo.martins@sou.inteli.edu.br>
2023-06-18 22:42:59 -0300

    refatoração-seçõesnovas
    Refatoração do código e adicionada duas páginas novas

--- src/Notebook/streamlit.py
+++ src/Notebook/streamlit.py
@@ -6,116 +6,172 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from io import BytesIO
 
-# Função para baixar csv
-def generate_csv_download_link(df):
-    towrite = BytesIO()
-    df.to_csv(towrite, encoding="utf-8", index=False, header=True)
-    towrite.seek(0)
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:text/csv;base64,{b64}" download="data_download.csv">Download CSV File</a>'
-    return st.markdown(href, unsafe_allow_html=True)
 
-# Título da página
-st.set_page_config(page_title='Chat-BTG')
-# Título da seção
-st.title('CSV Plotter')
-st.subheader('Coloque o seu arquivo CSV')
-# Variável para armazenar o arquivo CSV
-upload_file = st.file_uploader('Escolha o seu arquivo CSV', type='csv')
-# Condição para colocar na tela o arquivo CSV
-if upload_file:
-    st.markdown('---')
+# Função para renderizar a barra lateral com os links para as seções da sua aplicação
+def render_sidebar():
+    st.sidebar.title("Seções")
+    page = st.sidebar.radio("Navegue entre as seções", ["Dashboards", "Chat-Btg", "Predição"])
+    return page
+
+# Função para ler o arquivo CSV 
+def read_csv(upload_file):
     df = pd.read_csv(upload_file)
-    st.dataframe(df)
+    
+    return df
+#baixar csv
+def download_link(df, filename='predicoes.csv', link_text='Download CSV'):
+    """Gera um hyperlink para baixar um DataFrame em formato CSV"""
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{link_text}</a>'
+    return href
 
-    # Select box para os gráficos que gostaria de analisar
-    plot_select = st.selectbox(
-        'O que você gostaria de analisar?',
-        ('Porcentagem de Sentimento', 'Word Cloud', 'Número de Tokens por Sentimento', 'Emojis que mais aparecem')
+
+#Função para gerar gráfico de porcentagem de sentimento e histograma
+#df = dataframe
+#sentimento = coluna de sentimento
+def percentage_hist_sentiment(df, sentimento):
+    st.write('Gráfico de Pizza e Histograma')
+    # Porcentagens
+    # Positivo
+    total = len(df)
+    positivo = len(df.query(f'{sentimento} == 1')) / total
+    # Neutro
+    neutro = len(df.query(f'{sentimento} == 0')) / total
+    # Negativo
+    negativo = len(df.query(f'{sentimento} == -1')) / total
+    # Criar DataFrame para o gráfico de barras
+    # Plotar o gráfico de barras
+    fig = px.pie(df, values=[positivo, neutro, negativo], names=['Positivo', 'Neutro', 'Negativo'])
+    st.plotly_chart(fig)
+    #Histograma
+    labels = df[sentimento].map({1 :'Positivo', 0: 'Neutro', -1: 'Negativo'})
+    fig_hist = px.histogram(df, labels)
+    st.plotly_chart(fig_hist)
+
+#Gráfico word cloud positivo
+def word_cloud_positive(df, sentimento):
+    st.write(f"Gráfico Word Cloud de Frases {sentimento.capitalize()}s")
+    # Pegando apenas as frases com o sentimento correspondente
+    filtered_df = df.query(f'{sentimento} == 1')
+    # Transformando em tokens
+    tokens = filtered_df['texto_pre'].apply(lambda x: x.split())
+    # Array para armazenar
+    words = []
+    # Looping para salvar os tokens em um array
+    for sentences in tokens:
+        for token in sentences:
+            words.append(token)
+    # Concatenar tudo em uma string
+    text = ' '.join(words)
+
+    # Criar um objeto WordCloud
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate(text)
+
+    # Plotar a nuvem de palavras com Plotly Express
+    fig = px.imshow(wordcloud.to_array())
+    fig.update_layout(
+        showlegend=False, width=800, height=400, margin=dict(l=0, r=0, t=0, b=0),
+        xaxis=dict(visible=False), yaxis=dict(visible=False)
     )
+    st.plotly_chart(fig)
+    
+#Gráfico word cloud negativo
+def word_cloud_negative(df, sentimento):
+    st.write(f"Gráfico Word Cloud de Frases {sentimento.capitalize()}s")
+    # Pegando apenas as frases com o sentimento correspondente
+    filtered_df = df.query(f'{sentimento} == -1')
+    # Transformando em tokens
+    tokens = filtered_df['texto_pre'].apply(lambda x: x.split())
+    # Array para armazenar
+    words = []
+    # Looping para salvar os tokens em um array
+    for sentences in tokens:
+        for token in sentences:
+            words.append(token)
+    # Concatenar tudo em uma string
+    text = ' '.join(words)
 
-    # Mostrar gráfico correspondente à opção selecionada
-    if plot_select == 'Porcentagem de Sentimento':
-        st.write('Gráfico de Barras')
-        # Porcentagens
-        # Positivo
-        total = len(df)
-        positivo = len(df.query('sentimento == 1')) / total
-        # Neutro
-        neutro = len(df.query('sentimento == 0')) / total
-        # Negativo
-        negativo = len(df.query('sentimento == -1')) / total
-        # Criar DataFrame para o gráfico de barras
-        data = {
-            'Sentimento': ['Positivo', 'Neutro', 'Negativo'],
-            'Porcentagem': [positivo, neutro, negativo],
-            'Cor': ['#008000', '#ffff00', '#ff0000']
-        }
-        df_plot = pd.DataFrame(data)
-        # Plotar o gráfico de barras
-        fig = px.bar(df_plot, x='Sentimento', y='Porcentagem', color='Cor')
-        st.plotly_chart(fig)
+    # Criar um objeto WordCloud
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate(text)
 
-    elif plot_select == 'Word Cloud':
-        # Código para criar a nuvem de palavras
-        st.write("Gráfico Word Cloud de Frases Positivas")
-        # WordCloud das frases positivas
-        # Pegando apenas as frases com label positiva
-        pos = df.query('sentimento == 1')
-        # Transformando em tokens
-        tokens_pos = pos['texto_pre'].apply(lambda x: x.split())
-        # Array para armazenar
-        word_positive = []
-        # Looping para salvar os tokens em um array
-        for sentences in tokens_pos:
-            for tokens in sentences:
-                word_positive.append(tokens)
-        # Concatenar tudo em uma string
-        text_positive = ' '.join(word_positive)
+    # Plotar a nuvem de palavras com Plotly Express
+    fig = px.imshow(wordcloud.to_array())
+    fig.update_layout(
+        showlegend=False, width=800, height=400, margin=dict(l=0, r=0, t=0, b=0),
+        xaxis=dict(visible=False), yaxis=dict(visible=False)
+    )
+    st.plotly_chart(fig)
+    
+# Renderiza a barra lateral
+page = render_sidebar()
 
-        # Criar um objeto WordCloud
-        wordcloud_positive = WordCloud(width=800, height=400, background_color="white").generate(text_positive)
+# Mostra a página correspondente à opção selecionada na barra lateral
+if page == "Dashboards":
+    #Título Página
+    st.title("DASHBOARDS")
+    st.subheader('Coloque o seu arquivo CSV para gerar os gráficos')
+    # Variável para armazenar o arquivo CSV
+    upload_file = st.file_uploader('Escolha o seu arquivo CSV', type='csv')
+    # Condição para colocar na tela o arquivo CSV
+    if upload_file:
+        st.markdown('---')
+        # Lê os dados do arquivo CSV e armazena em um cache persistente
+        df = read_csv(upload_file)
+        st.dataframe(df)
 
-        # Plotar a nuvem de palavras com Plotly Express
-        fig_positive = px.imshow(wordcloud_positive.to_array())
-        fig_positive.update_layout(
-            showlegend=False, width=800, height=400, margin=dict(l=0, r=0, t=0, b=0),
-            xaxis=dict(visible=False), yaxis=dict(visible=False)
+        # Select box para os gráficos que gostaria de analisar
+        plot_select = st.selectbox(
+            'O que você gostaria de analisar?',
+            ('Porcentagem de Sentimento', 'Word Cloud', 'Número de Tokens por Sentimento', 'Emojis que mais aparecem')
         )
-        st.plotly_chart(fig_positive)
 
-        # Código para criar a nuvem de palavras
-        st.write("Gráfico Word Cloud de Frases Negativas")
-        # Filtrar o DataFrame pelo sentimento negativo
-        neg = df.query('sentimento == -1')
-        # Transformar as frases em tokens
-        tokens_neg = neg['texto_pre'].apply(lambda x: x.split())
-        # Lista para armazenar os tokens
-        word_negative = []
-        # Looping para salvar os tokens em uma lista
-        for sentences in tokens_neg:
-            for tokens in sentences:
-                word_negative.append(tokens)
-        # Concatenar tudo em uma string
-        text_negative = ' '.join(word_negative)
+        # Mostrar gráfico correspondente à opção selecionada
+        if plot_select == 'Porcentagem de Sentimento':
+            percentage_hist_sentiment(df, 'sentimento')
 
-        # Criar um objeto WordCloud com cor vermelha
-        wordcloud_negative = WordCloud(
-            width=800, height=400, background_color="white", colormap='Reds'
-        ).generate(text_negative)
+        elif plot_select == 'Word Cloud':
+            #word cloud positiva
+            word_cloud_positive(df, 'sentimento')
+            #word cloud negative
+            word_cloud_neagative(df, 'sentimento')
 
-        # Plotar a nuvem de palavras com Plotly Express
-        fig_negative = px.imshow(wordcloud_negative.to_array())
-        fig_negative.update_layout(
-            showlegend=False, width=800, height=400, margin=dict(l=0, r=0, t=0, b=0),
-            xaxis=dict(visible=False), yaxis=dict(visible=False)
-        )
-        st.plotly_chart(fig_negative)
 
-    elif plot_select == 'Número de Tokens por Sentimento':
-        # Código para criar o gráfico de barras
-        st.write("Gráfico KDE")
 
-    elif plot_select == 'Emojis que mais aparecem':
-        # Código para criar o gráfico de dispersão
-        st.write("Gráfico de barra")
+        elif plot_select == 'Número de Tokens por Sentimento':
+            # Código para criar o gráfico de barras
+            st.write("Gráfico KDE")
+
+        elif plot_select == 'Emojis que mais aparecem':
+            # Código para criar o gráfico de dispersão
+            st.write("Gráfico de barra")
+
+elif page == "Chat-Btg":
+    # Título da página
+    st.title('CHAT-BTG')
+    st.subheader('Teste o modelo de predição')
+    
+    with st.form(key='nlpForm'):
+        raw_text = st.text_area("Coloque seu texto aqui")
+        submit_button = st.form_submit_button(label='Analyze')
+
+
+elif page == "Predição":
+    st.title("PREDIÇÃO")
+    st.subheader("Faça a predição do seu arquivo CSV")
+    #Instanciando arquivo para importação
+    upload_file_2 = st.file_uploader('Escolha o seu arquivo CSV', type='csv')
+    #botão para executar a função
+    if upload_file_2:
+        df_2 = pd.read_csv(upload_file_2)
+        st.dataframe(df_2)
+        
+        pred_button = st.button(label='Predição')
+        if pred_button:
+            #Executar função de criar coluna com predição
+            #Lembrar de mudar df_2 para o dataframe com a coluna de predição
+            st.dataframe(df_2)
+            #Baixar CSV 
+            st.markdown(download_link(df_2), unsafe_allow_html= True)
+
+            
